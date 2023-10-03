2f52a64904f410a47edf6ec9873a0d763312bc60
sujan-shetty71 <113658538+sujan-shetty71@users.noreply.github.com>
2023-10-03 16:46:17 +0530

    Delete backend directory

--- backend/authServer/controller/authResponse.js
+++
@@ -1,8 +0,0 @@
-module.exports.authSuccessResponse = async function (req, res) {
-    res.status(200).json({
-        success: 'true',
-        token: req.body.token || req.query.token || req.headers["x-access-token"],
-        loginType: 'google',
-        isEmployee: 2,
-    })
-}
